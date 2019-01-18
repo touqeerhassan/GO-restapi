7de28fd5487ea97d8dbcdfa82e30c74948d24fe0
saqi <awanenterprices@Hassan-MacBook-Pro.local>
2019-01-18 10:45:59 +0500

    first commit

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+// State struct (Model)
+type State struct {
+	Type       string `json:"type"`
+	Icon       string `json:"icon"`
+	Title      string `json:"title"`
+	Value      string `json:"value"`
+	FooterText string `json:"footerText"`
+	FooterIcon string `json:"footerIcon"`
+}
+
+// User struct (Model)
+type User struct {
+	Name     string `json:"name"`
+	Password string `json:"password"`
+}
+
+// Init States var as a slice State struct
+var states []State
+
+// enable CORS
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+// Get all States
+func getStates(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+	states[0].Value = strconv.Itoa(random(10, 200)) + "GB"
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(states)
+}
+
+//authenticate User
+func authenticate(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var user User
+	_ = json.NewDecoder(r.Body).Decode(&user)
+	if user.Name == "john" && user.Password == "password" {
+		json.NewEncoder(w).Encode(user)
+	}
+}
+
+//generate random number between range
+func random(min, max int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max-min) + min
+}
+
+// Main function
+func main() {
+	// Init router
+	r := mux.NewRouter()
+	// Hardcoded data
+	states = append(states, State{Type: "warning", Icon: "ti-server", Title: "Capacity", FooterText: "Updated now", FooterIcon: "ti-reload"})
+	states = append(states, State{Type: "success", Icon: "ti-wallet", Title: "Revenue", Value: "$1,345", FooterText: "Last day", FooterIcon: "ti-calendar"})
+	states = append(states, State{Type: "danger", Icon: "ti-pulse", Title: "Errors", Value: "23", FooterText: "In the last hour", FooterIcon: "ti-timer"})
+	states = append(states, State{Type: "info", Icon: "ti-twitter-alt", Title: "Followers", Value: "+45", FooterText: "Updated now", FooterIcon: "ti-reload"})
+
+	// Route handles & endpoints
+	r.HandleFunc("/states", getStates).Methods("GET")
+	r.HandleFunc("/authenticate", authenticate).Methods("POST")
+
+	// Start server
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
